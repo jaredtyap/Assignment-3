34dfdfe60e357bd6e451df0adb3afbc2d0bd9e0e
jaredtyap <53233390+jaredtyap@users.noreply.github.com>
2021-05-08 00:15:52 -0700

    Add files via upload
    changes to comparing loop

--- main.py
+++ main.py
@@ -13,21 +13,18 @@ total_doc = 0
 for filename in os.listdir(path):
     with open(os.path.join(path,filename),'r') as f:
         #file = open(f,)
-        print(f)
         total_doc +=1
         data = json.load(f)
         soupdata = data['content']
-        print(data['url'])
         soup = BeautifulSoup(soupdata, 'html.parser')
         raw_data = soup.get_text()
         tokenizer = nltk.RegexpTokenizer(r'\w{2,}')
         tokens = tokenizer.tokenize(raw_data)
-        for x in tokens: 
+        for x in tokens:
             x = x.lower()
             x = ps.stem(x)
             flag=0
-            for i in list(index):
-                if x == i:
+            if x in index.items():
                     index[x] +=1
                     flag=1
                     break
