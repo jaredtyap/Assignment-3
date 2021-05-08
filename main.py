b834522b46ae1ec818f20656d1ac5c5e70ba99ed
jaredtyap <53233390+jaredtyap@users.noreply.github.com>
2021-05-07 21:03:01 -0700

    Add files via upload
    added stem on the token and updated adding to dictionary

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import json
 import os
 
 ps = PorterStemmer()
-path = 'testing/'
+path = 'source/'
 index = {}
 index['the'] = 0
 total_doc = 0
 for filename in os.listdir(path):
     with open(os.path.join(path,filename),'r') as f:
         #file = open(f,)
+        print(f)
         total_doc +=1
         data = json.load(f)
         soupdata = data['content']
@@ -21,20 +22,22 @@ for filename in os.listdir(path):
         raw_data = soup.get_text()
         tokenizer = nltk.RegexpTokenizer(r'\w{2,}')
         tokens = tokenizer.tokenize(raw_data)
-        for x in tokens:
+        for x in tokens: 
             x = x.lower()
+            x = ps.stem(x)
             flag=0
-
             for i in list(index):
                 if x == i:
                     index[x] +=1
                     flag=1
-            
+                    break
             if flag == 0:
                 index[x] = 1
             else: 
                 flag = 0
-                
-
-print(dict(sorted(index.items(), key=lambda item: item[1])))
+        print(total_doc)
+f = open("index.txt","w", encoding="utf8")
+f.write(str(index))
+f.close()
+print(len(index))
 print(total_doc)
